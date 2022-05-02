91715d56a3a61f29217781bd1223f6398a72b9ab
leyooow <leoespino999@gmail.com>
2022-05-02 22:04:17 +0800

    new

--- firebase-angular/src/app/app.component.ts
+++ firebase-angular/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { 
-Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword,
-} from '@angular/fire/auth'
 
 @Component({
   selector: 'app-root',
@@ -10,18 +7,4 @@ Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword,
 })
 export class AppComponent {
   title = 'firebase-angular';
-
-  constructor(public auth: Auth){
-
-  }
-
-  handleRegister(value: any){
-    createUserWithEmailAndPassword(this.auth, value.email, value.password)
-    .then((response: any) => {
-      console.log(response.user)
-    })
-    .catch((err) => {
-      alert(err.message)
-    })
-  }
 }
--- firebase-angular/src/app/app.module.ts
+++ firebase-angular/src/app/app.module.ts
@@ -3,37 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
-    FormsModule,
- 
-  
-
-    
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
